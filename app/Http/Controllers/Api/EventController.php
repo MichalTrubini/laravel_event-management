4e6c8de99a213956baebe9fbb48e2f9c91a92810
Michal Trubini <mchl.trubini@gmail.com>
2024-01-26 15:52:40 +0100

    wip

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use Illuminate\Http\Request;
 use App\Models\Event;
 
@@ -12,13 +13,29 @@ class EventController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-    {
-        return Event::all();
+    {   
+        $query = Event::query();
+        $relations = ['user', 'attendees', 'attendees.user'];
+
+        foreach ($relations as $relation) {
+            $query->when($this->shouldIncludeRelation($relation), function ($query) use ($relation) {
+                $query->with($relation);
+            });
+        }
+        return EventResource::collection($query->latest()->paginate());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    protected function shouldIncludeRelation(string $relation) : bool {
+        $include = request()->query('include');
+
+        if (!$include) {
+            return false;
+        }
+
+        $relations = array_map('trim', explode(',', $include)) ;
+        return in_array($relation, $relations); 
+    }
+
     public function store(Request $request)
     {
         $validatedData = $request->validate([
@@ -33,30 +50,41 @@ class EventController extends Controller
     
         $event = Event::create($validatedData);
     
-        return $event;
+        return new EventResource($event);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Event $event)
     {
-        return $event = Event::find($id);
+        $event->load('user', 'attendees');
+        return new EventResource($event);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
+
+        $event->update($request->validate([
+            'name' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+            'start_date' => 'sometimes|date',
+            'end_date' => 'sometimes|date|after:start_date',
+        ]));
+
+        new EventResource($event);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(event $event)
     {
-        //
+        $event->delete();
+
+        return response()->json(['message' => 'Event deleted']);
     }
 }
